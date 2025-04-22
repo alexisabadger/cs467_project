702b5c8798d5a4bafd2c34ad993842be83f60b2d
Eddy Song <songjre@oregonstate.edu>
2025-04-21 22:57:09 -0700

    Added dashboard

---
+++ personal-trainer/components/MainContent.tsx
@@ -0,0 +1,12 @@
+export default function MainContent() {
+    return (
+      <main style={{}}>
+        <h1>Daily Workout Plan</h1>
+          <ul>
+            <li>Line 1</li>
+            <li>Line 2</li>
+            <li>Line 3</li>
+          </ul>
+      </main>
+    );
+  }
