4700f71ed604f8d4d97252bc76ebaac98fb24de3
Alex McIntire <imalexmcintire@gmail.com>
2025-05-19 21:57:59 -0400

    EAM - fix: compilation error --> commented out unused import

--- personal-trainer/components/LeftSidebar.tsx
+++ personal-trainer/components/LeftSidebar.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import SurveyCard from './SurveyCard';
-import Link from 'next/link';
+// import Link from 'next/link';
 import styles from '@/components/Dashboard.module.css';
 
 
